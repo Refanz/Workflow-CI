af8529e886363ef935e937840b566f23ccdb527a
Refanz <refandasuryasaputra@gmail.com>
2025-06-11 00:27:47 +0700

    update code modelling.py using env for hyperparameter and dataset

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,35 +1,54 @@
+import os.path
+import sys
+import warnings
+
 import mlflow
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
-mlflow.set_experiment("SMSML")
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
 
-# Load dataset
-mushroom_df = pd.read_csv("mushrooms_preprocessed.csv")
+    # Load dataset
+    dataset_path = sys.argv[4] if len(sys.argv) > 4 else os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                                      "mushrooms_preprocessed.csv")
+    mushroom_df = pd.read_csv(dataset_path)
 
-# Melakukan splitting dataset
-X = mushroom_df.drop(columns=['class'], axis=1)
-y = mushroom_df['class']
+    # Melakukan splitting dataset
+    X = mushroom_df.drop(columns=['class'], axis=1)
+    y = mushroom_df['class']
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-# Menyimpan sample input
-input_example = X_train[0:5]
+    # Menyimpan sample input
+    input_example = X_train[0:5]
 
-# Melatih model dan menjalankan mlflow
-with mlflow.start_run():
-    # Mengaktifkan autologging
-    mlflow.autolog()
+    # Hyperparameter
+    solver = sys.argv[1] if len(sys.argv) > 1 else "liblinear"
+    penalty = sys.argv[2] if len(sys.argv) > 2 else "l1"
+    C = int(sys.argv[3]) if len(sys.argv) > 3 else 10
 
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
+    # Melatih model dan menjalankan mlflow
+    with mlflow.start_run():
+        model = LogisticRegression(
+            solver=solver,
+            penalty=penalty,
+            C=C,
+            random_state=42,
+        )
+        model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
+        y_pred = model.predict(X_test)
 
-    test_score = model.score(X_test, y_test)
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=input_example,
+        )
 
-    print(f"Accuracy Score Test: {test_score}")
+        # Log metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
